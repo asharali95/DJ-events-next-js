ca752fb3944890969463b6b05c0583ba895ccf07
asharali95 <ashar.alia1999@gmail.com>
2022-04-14 14:49:13 +0500

    header & footer component + about page update

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
+import Layout from "../Components/Layout/Layout";
+
 export default function AboutPage() {
   return (
-    <div>
-      <h1>This is About Page</h1>
-      <p>This page is to help to find DJ musics </p>
-    </div>
+    <Layout title="About">
+      <div>
+        <h1>About</h1>
+        <p>This page is to help to find DJ musics </p>
+        <p>version 1.0.0</p>
+      </div>
+    </Layout>
   );
 }
