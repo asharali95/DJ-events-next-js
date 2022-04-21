e3a24242365291adffe2d6e428e9f41c82188211
asharali95 <ashar.alia1999@gmail.com>
2022-04-21 21:29:06 +0500

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Layout from "../Components/Layout/Layout";
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Home</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3000/api/events");
+  const events = await response.json();
+  console.log(events);
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
