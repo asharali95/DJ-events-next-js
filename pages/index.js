a5fdd927e6c267d442169e613f3640351214c446
asharali95 <ashar.alia1999@gmail.com>
2022-04-23 23:41:00 +0500

    event displaying (2)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import EventItems from "Components/EventItems/EventItems";
 import Layout from "../Components/Layout/Layout";
+import Link from "next/Link";
 export default function Home({ events }) {
   return (
     <Layout>
@@ -8,14 +9,20 @@ export default function Home({ events }) {
       {events.map((event) => (
         <EventItems key={event.id} event={event} />
       ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn">More events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch("http://localhost:3000/api/events");
   const { events } = await response.json();
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
   };
 }
